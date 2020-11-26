3a078fa7c7a196cfe0ecb11625ea8392cd56588d
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-11-26 17:17:07 +0000

    the emotional actor

---
+++ src/main/scala/statefulAndstateless/TypedStatelessActors.scala
@@ -0,0 +1,46 @@
+package statefulAndstateless
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+
+object TypedStatelessActors {
+
+  // the trait plus the possible message an actor can accept
+  trait SimpleThing
+  case object EatChocolate extends SimpleThing
+  case object WashDishes extends SimpleThing
+  case object LearnAkka extends SimpleThing
+
+  val emotionalMutableActor: Behavior[SimpleThing] = Behaviors.setup{ context =>
+    // spin up the actor state
+    var happiness = 0
+
+
+    // behavior of the actor
+    Behaviors.receiveMessage{
+      case EatChocolate =>
+        context.log.info(s"($happiness) Eating chocolate, getting a shot of dopamine")
+        happiness += 1
+        Behaviors.same
+      case WashDishes =>
+        context.log.info(s"($happiness) Doing a chore, womp, womp...")
+        happiness -= 2
+        Behaviors.same
+      case LearnAkka =>
+        context.log.info(s"($happiness) Learning Akka, this is cool!")
+        happiness += 100
+        Behaviors.same
+      case _ =>
+        context.log.info(s"($happiness) Received something I don't know")
+        Behaviors.same
+
+    }
+  }
+
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
