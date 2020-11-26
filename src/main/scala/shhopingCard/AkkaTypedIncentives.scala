6b7fcf3843ffda11f1708b36634df015fcbc5c73
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-11-26 16:06:23 +0000

    shopping typed actor specifying the type of messages which it can receive

--- src/main/scala/shhopingCard/AkkaTypedIncentives.scala
+++ src/main/scala/shhopingCard/AkkaTypedIncentives.scala
@@ -1,6 +1,7 @@
 package shhopingCard
 
 import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
 
 object AkkaTypedIncentives {
 
@@ -10,11 +11,25 @@ object AkkaTypedIncentives {
   case class RemoveItem(item: String) extends ShoppingCardMessage
   case class ValidateCard(item: String) extends ShoppingCardMessage
 
+  val shoppingRootActor = ActorSystem(
+    Behaviors.receiveMessage[ShoppingCardMessage]{ message: ShoppingCardMessage =>
+      message match {
+        case AddItem(item) => println(s"Adding $item to card")
+        case RemoveItem(item) => println(s"Removing $item from card")
+        case ValidateCard(item) => println("The card is good.")
+      }
+      Behaviors.same
+    },
+    "ShoppingActor"
+  )
+
+
+
   // 2 - mutable state
 
   // 3 - hierarchy
 
 
-  val actor = ActorSystem
+
 
 }
