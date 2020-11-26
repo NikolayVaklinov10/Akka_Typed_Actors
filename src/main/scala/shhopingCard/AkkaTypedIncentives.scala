a3ed9a88211871a839da2a1b7133f9830c3c351c
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-11-26 16:32:45 +0000

    the version of the shopping card actor, but with mutable state in it

--- src/main/scala/shhopingCard/AkkaTypedIncentives.scala
+++ src/main/scala/shhopingCard/AkkaTypedIncentives.scala
@@ -1,6 +1,6 @@
 package shhopingCard
 
-import akka.actor.typed.ActorSystem
+import akka.actor.typed.{ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 
 object AkkaTypedIncentives {
@@ -24,12 +24,45 @@ object AkkaTypedIncentives {
   )
   shoppingRootActor ! ValidateCard
 
-
-
   // 2 - mutable state
+  val shoppingRootActorMutable = ActorSystem(
+    Behaviors.setup[ShoppingCardMessage]{ ctx =>
+      // local state = mutable
+      var items: Set[String] = Set()
+
+      Behaviors.receiveMessage[ShoppingCardMessage]{ message: ShoppingCardMessage =>
+        message match {
+          case AddItem(item) =>
+            println(s"Adding $item to card")
+            // mutating a variable
+            items += item
+          case RemoveItem(item) =>
+            println(s"Removing $item from card")
+            items -= item
+          case ValidateCard =>
+            println("The card is good.")
+        }
+        Behaviors.same
+      }
+    },
+    "ShoppingActorMutable"
+  )
+
+  def shoppingBehavior(items: Set[String]): Behavior[ShoppingCardMessage] =
+    Behaviors.receiveMessage[ShoppingCardMessage] {
+      case AddItem(item) =>
+        println(s"Adding $item to card")
+        // mutating a variable
+        shoppingBehavior(items + item)
+      case RemoveItem(item) =>
+        println(s"Removing $item from card")
+        shoppingBehavior(items - item)
+      case ValidateCard =>
+        println("The card is good.")
+        Behaviors.same
+    }
 
   // 3 - hierarchy
-
 
 
 
