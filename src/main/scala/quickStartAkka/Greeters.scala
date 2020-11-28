d5342b6f8bb5e7fceb3d39540990918c7c79bfe7
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-11-28 11:30:25 +0000

    the greeter file

---
+++ src/main/scala/quickStartAkka/Greeters.scala
@@ -0,0 +1,29 @@
+package quickStartAkka
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+
+
+// #greeter-actor
+object Greeter {
+  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
+  final case class Greeted(whom: String, from: ActorRef[Greet])
+
+  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
+    context.log.info("Hello {}!", message.whom)
+    //#greeter-send-messages
+    message.replyTo ! Greeted(message.whom, context.self)
+    //#greeter-send-messages
+    Behaviors.same
+  }
+}
+
+
+
+
+object Greeters extends App {
+
+
+
+
+}
