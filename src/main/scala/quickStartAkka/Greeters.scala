af06241ffeb49d74436fe5e09c1432d0f40fabd6
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-11-28 12:05:15 +0000

    the greeter main actor which starts the others actor by spawning them

--- src/main/scala/quickStartAkka/Greeters.scala
+++ src/main/scala/quickStartAkka/Greeters.scala
@@ -18,6 +18,46 @@ object Greeter {
   }
 }
 
+//#greeter-bot
+object GreeterBot {
+
+  def apply(max: Int): Behavior[Greeter.Greeted] = {
+    bot(0, max)
+  }
+
+  private def bot(greetingCounter: Int, max: Int): Behavior[Greeter.Greeted] =
+    Behaviors.receive { (context, message) =>
+      val n = greetingCounter + 1
+      context.log.info("Greeting {} for {}", n, message.whom)
+      if (n == max) {
+        Behaviors.stopped
+      } else {
+        message.from ! Greeter.Greet(message.whom, context.self)
+        bot(n, max)
+      }
+    }
+}
+
+//#greeter-main
+object GreeterMain {
+
+  final case class SyHello(name: String)
+
+  def apply(): Behavior[SyHello] =
+    Behaviors.setup { context =>
+      //#create-actors
+      val greeter = context.spawn(Greeter(), "greeter")
+
+      Behaviors.receiveMessage { message =>
+        //#create-actors
+        val replyTo = context.spawn(GreeterBot(max=3), message.name)
+        greeter ! Greeter.Greet(message.name, replyTo)
+        Behaviors.same
+      }
+    }
+}
+
+
 
 
 
