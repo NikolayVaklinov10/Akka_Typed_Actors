ca77047ba7e950ee0ae70072d8bae90493e159c5
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-02 21:39:55 +0000

    several tests for the akka shopping application to be implemented

---
+++ src/test/scala/shhopingCard/AkkaTypedIncentivesTest.scala
@@ -0,0 +1,21 @@
+package shhopingCard
+
+class AkkaTypedIncentivesTest extends org.scalatest.FunSuite {
+
+  test("testRootOnlineStoreActor") {
+
+  }
+
+  test("testShoppingRootActorMutable") {
+
+  }
+
+  test("testShoppingBehavior") {
+
+  }
+
+  test("testShoppingRootActor") {
+
+  }
+
+}
