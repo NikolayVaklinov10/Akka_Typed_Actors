f6c717212beb186390ac52dd5f29d5ec0baeebe3
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-04 21:55:35 +0000

    actor test for the typed stateless actor with more code to be added

---
+++ src/test/scala/statefulAndstateless/TypedStatelessSpec.scala
@@ -0,0 +1,15 @@
+package statefulAndstateless
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.wordspec.AnyWordSpecLike
+import statefulAndstateless.TypedStatelessActors.SimpleThing
+
+class TypedStatelessSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+ "A TypeStateless Actor" must {
+   "increase or decrease" in {
+
+
+   }
+ }
+}
